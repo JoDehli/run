602c8bbdfada84d55995d614612c8927e81862cf
Luke Johnson <luke.johnson9423@gmail.com>
2020-04-22 12:29:04 -0400

    basic arguments and flags handling

--- lib/main.go
+++ lib/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	// Arguments
+	arguments := os.Args[1:]
+
+	// Flags
+	v := flag.Bool("v", false, "Prints the current installed version of this tool.")
+
+	// Parse flags
+	flag.Parse()
+
+	// Too many arguments
+	if len(arguments) > 1 {
+		fmt.Println("Too many arguments.")
+		fmt.Println("Run \"do -h\" for usage instructions.")
+		os.Exit(2)
+	}
+
+	// No arguments - output usage instructions
+	if len(arguments) == 0 {
+		flag.Usage()
+		os.Exit(0)
+	}
+
+	// Run version command
+	if *v {
+		fmt.Println("version")
+		os.Exit(0)
+	}
+
+	script := arguments[0]
+	fmt.Println(script)
 }
