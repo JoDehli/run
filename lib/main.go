cd39eae1ba0f3d5b8a8cad80da316898cef24cdd
Luke Johnson <luke.johnson9423@gmail.com>
2020-04-24 09:30:55 -0400

    style: fixing main.go indention

--- lib/main.go
+++ lib/main.go
@@ -1,41 +1,41 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"os"
+  "flag"
+  "fmt"
+  "os"
 
-	"github.com/lukecjohnson/dot/lib/commands"
+  "github.com/lukecjohnson/dot/lib/commands"
 )
 
 func main() {
-	arguments := os.Args[1:]
+  arguments := os.Args[1:]
 
-	if len(arguments) > 1 {
-		fmt.Println("Error: Too many arguments.")
-		fmt.Println("Run \"dot --help\" for usage instructions.")
-		os.Exit(2)
-	}
+  if len(arguments) > 1 {
+    fmt.Println("Error: Too many arguments.")
+    fmt.Println("Run \"dot --help\" for usage instructions.")
+    os.Exit(2)
+  }
 
-	if len(arguments) == 0 {
-		commands.Help()
-	}
+  if len(arguments) == 0 {
+    commands.Help()
+  }
 
-	flag.Usage = commands.Help
+  flag.Usage = commands.Help
 
-	versionFlag := flag.Bool("version", false, "Reports the current installed version of dot.")
-	listFlag := flag.Bool("list", false, "Lists all the available commands found in \"dot.yaml\".")
+  versionFlag := flag.Bool("version", false, "Reports the current installed version of dot.")
+  listFlag := flag.Bool("list", false, "Lists all the available commands found in \"dot.yaml\".")
 
-	flag.Parse()
+  flag.Parse()
 
-	if *versionFlag {
-		commands.Version()
-	}
+  if *versionFlag {
+    commands.Version()
+  }
 
-	if *listFlag {
-		commands.List()
-	}
+  if *listFlag {
+    commands.List()
+  }
 
-	command := arguments[0]
-	commands.Run(command)
+  command := arguments[0]
+  commands.Run(command)
 }
