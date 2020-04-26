b16408da2157588984d22f13973f096875c59b0b
Luke Johnson <luke.johnson9423@gmail.com>
2020-04-25 22:58:04 -0400

    fix: check if dot.yaml exists before trying to read

--- lib/utils/parse-yaml.go
+++ lib/utils/parse-yaml.go
@@ -9,15 +9,20 @@ import (
 )
 
 func ParseYaml(filename string) map[string]string {
+  if _, err := os.Stat(filename); os.IsNotExist(err) {
+    fmt.Printf("Error: Unable to find \"%s\" in the current directory. \n", filename)
+    os.Exit(1)
+  }
+
   data, err := ioutil.ReadFile(filename)
   if err != nil {
-    fmt.Printf("Error: Cannot find \"%s\" in the current directory.", filename)
+    fmt.Printf("Error: Unable to read \"%s\". \n", filename)
     os.Exit(1)
   }
 
   yamlMap := make(map[string]string)
   if err := yaml.Unmarshal([]byte(data), &yamlMap); err != nil {
-    fmt.Printf("Error: Unable to parse \"%s\".", filename)
+    fmt.Printf("Error: Unable to parse \"%s\". \n", filename)
     os.Exit(1)
   }
 
