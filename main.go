3b4585d351e12325dd3c14cd43154fbb777e5ff7
Luke Johnson <luke.johnson9423@gmail.com>
2020-06-18 22:38:04 -0400

    adding check for both run.yaml and run.yml

--- main.go
+++ main.go
@@ -1,27 +1,24 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
-	"os"
-	"os/exec"
+  "fmt"
+  "io/ioutil"
+  "os"
+  "os/exec"
 
-	flag "github.com/spf13/pflag"
-	"gopkg.in/yaml.v2"
+  flag "github.com/spf13/pflag"
+  "gopkg.in/yaml.v2"
 )
 
-// Version : Dynamically set at build time to the most recent git tag
+// Dynamically set at build time to the most recent git tag
 var Version = "DEV"
 
 func main() {
-  // Arguments
   arguments := os.Args[1:]
 
-  // Flags
   versionFlag := flag.BoolP("version", "v", false, "Reports the current installed version of run.")
   listFlag := flag.BoolP("list", "l", false, "Lists all the available commands found in \"run.yaml\".")
 
-  // Override default usage output
   flag.Usage = func() {
     fmt.Println("Usage:")
     fmt.Println("  run [command]")
@@ -32,48 +29,44 @@ func main() {
     os.Exit(0)
   }
 
-  // Parse flags
   flag.Parse()
 
-  // Too many arguments
   if len(arguments) > 1 {
     fmt.Println("Error: too many arguments.")
     fmt.Println("Run \"run --help\" for usage instructions.")
     os.Exit(2)
   }
 
-  // No arguments/flags - show usage instructions
   if len(arguments) == 0 {
     flag.Usage()
   }
 
-  // Version flag - print version
   if *versionFlag {
     fmt.Println(Version)
     os.Exit(0)
   }
 
-  // Check for "run.yaml"
-  if _, err := os.Stat("run.yaml"); os.IsNotExist(err) {
-    fmt.Println("Error: unable to resolve \"run.yaml\" in the current directory.")
-    os.Exit(1)
+  commandsFile := "run.yaml"
+  if _, err := os.Stat(commandsFile); os.IsNotExist(err) {
+    commandsFile = "run.yml"
+    if _, err := os.Stat(commandsFile); os.IsNotExist(err) {
+      fmt.Println("Error: unable to resolve \"run.yaml\" in the current directory.")
+      os.Exit(1)
+    }
   }
 
-  // Read "run.yaml"
-  data, err := ioutil.ReadFile("run.yaml")
+  data, err := ioutil.ReadFile(commandsFile)
   if err != nil {
     fmt.Println("Error: unable to read \"run.yaml\".")
     os.Exit(1)
   }
 
-  // Parse "run.yaml"
   commands := make(map[string]string)
   if err := yaml.Unmarshal(data, &commands); err != nil {
     fmt.Println("Error: unable to parse \"run.yaml\".")
     os.Exit(1)
   }
 
-  // List flag - print available commands
   if *listFlag {
     fmt.Println("Available commands:")
     for command := range commands {
@@ -83,16 +76,13 @@ func main() {
     os.Exit(0)
   }
 
-  // Provided command
   command := arguments[0]
 
-  // Check if command exists in "run.yaml"
   if _, ok := commands[command]; !ok {
     fmt.Printf("Error: command \"%s\" cannot be found in \"run.yaml\".\n", command)
     os.Exit(2)
   }
 
-  // Execute command
   cmd := exec.Command("/bin/sh", "-c", commands[command])
   cmd.Stdout = os.Stdout
   cmd.Stderr = os.Stderr
