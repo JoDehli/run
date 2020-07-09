14200522f364ce7d24989e58a6d0c387664134c6
Luke Johnson <luke.johnson9423@gmail.com>
2020-07-08 20:02:01 -0400

    add support for chaining multiple commands together

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -32,66 +33,97 @@ func main() {
 	// Parse flags
 	flag.Parse()
 
-	// Parse non-flag arguments
-	args := flag.Args()
-
-	if len(args) == 0 {
-		flag.Usage()
-	}
-
-	if len(args) > 1 {
-		fmt.Println("Error: too many arguments.")
-		fmt.Println("Run \"run --help\" for usage instructions.")
-		os.Exit(2)
-	}
-
 	if *version {
 		fmt.Println(currentVersion)
 		os.Exit(0)
 	}
 
-	commandsFile := "run.yaml"
-	if _, err := os.Stat(commandsFile); os.IsNotExist(err) {
-		commandsFile = "run.yml"
-		if _, err := os.Stat(commandsFile); os.IsNotExist(err) {
-			fmt.Println("Error: unable to find \"run.yaml\" in the current directory.")
+	if *list {
+		if err := printAvailableCommands(); err != nil {
+			fmt.Println(err)
 			os.Exit(1)
 		}
 	}
 
-	data, err := ioutil.ReadFile(commandsFile)
-	if err != nil {
-		fmt.Println("Error: unable to read \"run.yaml\".")
+	// Parse non-flag arguments (commands)
+	args := flag.Args()
+
+	if len(args) == 0 {
+		if err := printAvailableCommands(); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+
+	if err := executeCommands(args); err != nil {
+		fmt.Println(err)
 		os.Exit(1)
+	}
+}
+
+func getAvailableCommands() (map[string]string, error) {
+	file := "run.yaml"
+	if _, err := os.Stat(file); os.IsNotExist(err) {
+		file = "run.yml"
+		if _, err := os.Stat(file); os.IsNotExist(err) {
+			return nil, errors.New("unable to find \"run.yaml\" in the current directory")
+		}
+	}
+
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil, errors.New("unable to read \"run.yaml\"")
 	}
 
 	commands := make(map[string]string)
 	if err := yaml.Unmarshal(data, &commands); err != nil {
-		fmt.Println("Error: unable to parse \"run.yaml\".")
-		os.Exit(1)
+		return nil, errors.New("unable to parse \"run.yaml\"")
 	}
 
-	if *list {
-		fmt.Println("Available commands:")
-		for command := range commands {
-			fmt.Println("  " + command)
+	return commands, nil
+}
+
+func printAvailableCommands() error {
+	availableCommands, err := getAvailableCommands()
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("\nAvailable commands:")
+	for c := range availableCommands {
+		fmt.Printf("- %s\n", c)
+	}
+	fmt.Println()
+
+	return nil
+}
+
+func executeCommands(commands []string) error {
+	availableCommands, err := getAvailableCommands()
+	if err != nil {
+		return err
+	}
+
+	commandString := ""
+
+	for i, c := range commands {
+		if _, ok := availableCommands[c]; !ok {
+			return fmt.Errorf("command \"%s\" could not be found", c)
 		}
 
-		os.Exit(0)
+		commandString += availableCommands[c]
+		if i+1 < len(commands) {
+			commandString += " && "
+		}
 	}
 
-	command := args[0]
-
-	if _, ok := commands[command]; !ok {
-		fmt.Printf("Error: command \"%s\" cannot be found in \"run.yaml\".\n", command)
-		os.Exit(2)
-	}
-
-	cmd := exec.Command("/bin/sh", "-c", commands[command])
+	cmd := exec.Command("/bin/sh", "-c", commandString)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	if err := cmd.Run(); err != nil {
-		fmt.Printf("Error: failed to execute \"%s\".\n", commands[command])
-		os.Exit(1)
+		fmt.Println()
+		return errors.New("failed to execute command")
 	}
+
+	return nil
 }
